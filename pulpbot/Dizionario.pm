0635aa80fb0e5cbec5a80012fdd33d37dea6e46e
giuseppe <giuseppe>
2004-08-03 10:28:15 +0000

    Un po' di stile

--- pulpbot/Dizionario.pm
+++ pulpbot/Dizionario.pm
@@ -4,101 +4,78 @@ use Exporter;
 @EXPORT_OK=qw(cerca);
 
 sub cerca {
-	#@temp= split /\ /, @_;
-	$stringa = $_[0];
-	$numero_lemma = $_[1]; 
-	$numero_pagina=`curl http://www.demauroparavia.it/cerca?stringa=$stringa`; 
-	if(!($numero_pagina =~ /Non ho trovato occorrenze/))
-	{
-		if($numero_pagina =~ /URL/)
-		{
-			#print("un solo lemma");
-			@righe= split /\n/, $numero_pagina;
-			foreach $linea (@righe)
-			{
-				if($linea =~ /URL/)
-				{
-					$inizio = index($linea , "URL")+4;
-					$numero_lemma = substr($linea, $inizio,10);
-					$numero_lemma =~ s/\D//gs;
-		
-				}
-			}
+    #@temp= split /\ /, @_;
+    $stringa = $_[0];
+    $numero_lemma = $_[1]; 
+    $numero_pagina=`curl http://www.demauroparavia.it/cerca?stringa=$stringa`; 
+    if(!($numero_pagina =~ /Non ho trovato occorrenze/)) {
+	if($numero_pagina =~ /URL/) {
+	    #print("un solo lemma");
+	    @righe= split /\n/, $numero_pagina;
+	    foreach $linea (@righe) {
+		if($linea =~ /URL/) {
+		    $inizio = index($linea , "URL")+4;
+		    $numero_lemma = substr($linea, $inizio,10);
+		    $numero_lemma =~ s/\D//gs;
 		}
-		else
-		{
-			$definizione="Ho trovato più di un lemma per $stringa\n";
-			$inizio=index($numero_pagina, "Ricerca");
-			$numero_pagina=substr($numero_pagina, $inizio);
-			$inizio=index($numero_pagina, "<a href=\"http");
-			$numero_pagina=substr($numero_pagina, 0, $inizio);
-			$inizio=index($numero_pagina, "<table");
-			$numero_pagina=substr($numero_pagina, $inizio);
-			@lemmi = split /<\/td><\/tr>/, $numero_pagina;
-			if(length($numero_lemma)>0)
-			{
-				foreach $lemma (@lemmi)
-				{
-					if($lemma =~ /<sup>$numero_lemma<\/sup>/)
-					{
-						$inizio=index($lemma, "href=")+5;
-						$numero_lemma=substr($lemma, $inizio, 10);
-						$numero_lemma =~ s/\D//gs;
-					}
-				}
-			}
-			else
-			{
-				foreach $lemma (@lemmi)
-				{
-					if($lemma =~/<sup>[0-9]<\/sup>/)
-					{
-						$inizio=index($lemma, "<sup>");
-						$lemma=substr($lemma, $inizio);
-						$lemma =~ s/&nbsp;/\ /g;
-						$lemma =~ s/<[^<]*>/\ /g;
-						#print $lemma."\n";
-						$definizione=$lemma;
-					}
-				}
-			}
-	
+	    }
+	} else {
+	    $definizione="Ho trovato più di un lemma per $stringa\n";
+	    $inizio=index($numero_pagina, "Ricerca");
+	    $numero_pagina=substr($numero_pagina, $inizio);
+	    $inizio=index($numero_pagina, "<a href=\"http");
+	    $numero_pagina=substr($numero_pagina, 0, $inizio);
+	    $inizio=index($numero_pagina, "<table");
+	    $numero_pagina=substr($numero_pagina, $inizio);
+	    @lemmi = split /<\/td><\/tr>/, $numero_pagina;
+	    if(length($numero_lemma)>0) {
+		foreach $lemma (@lemmi)	{
+		    if($lemma =~ /<sup>$numero_lemma<\/sup>/) {
+			$inizio=index($lemma, "href=")+5;
+			$numero_lemma=substr($lemma, $inizio, 10);
+			$numero_lemma =~ s/\D//gs;
+		    }
 		}
+	    } else {
+		foreach $lemma (@lemmi)	{
+		    if($lemma =~/<sup>[0-9]<\/sup>/) {
+			$inizio=index($lemma, "<sup>");
+			$lemma=substr($lemma, $inizio);
+			$lemma =~ s/&nbsp;/\ /g;
+			$lemma =~ s/<[^<]*>/\ /g;
+			#print $lemma."\n";
+			$definizione=$lemma;
+		    }
+		}
+	    }
 	}
-	else
-	{
-		$definizione="Lemma $stringa non trovato\n";
-	}
+    } else {
+	$definizione="Lemma $stringa non trovato\n";
+    }
 	
-	if($numero_lemma)
-	{
-		$pagina=`curl http://www.demauroparavia.it/$numero_lemma | grep -a class=lemma`; 
-		
-		if(length($pagina)>1500)
-		{
-			$pagina=substr($pagina,0,1500);
-			$cont=1;
-		}
-		@contenuto = split /<\/td><\/tr>/, $pagina;
-		$pagina="";
-		foreach $linea (@contenuto) {
-			$linea =~ s/<\/td><\/tr>/\n/;
-			$linea =~ s/&nbsp;/\ /g;
-			$linea =~ s/<[^<]*>//g;
-			if(length($linea)>0)
-			{
-				#print $linea."\n";
-				$definizione=$linea
-			}
-		}
-		if($cont)
-		{
-			$definizione="continua su http://www.demauroparavia.it/$numero_lemma\n";
-		}
+    if($numero_lemma)
+    {
+	$pagina=`curl http://www.demauroparavia.it/$numero_lemma | grep -a class=lemma`; 
+	if(length($pagina)>1500) {
+	    $pagina=substr($pagina,0,1500);
+	    $cont=1;
 	}
-	
-	return $definizione;
-
+	@contenuto = split /<\/td><\/tr>/, $pagina;
+	$pagina="";
+	foreach $linea (@contenuto) {
+	    $linea =~ s/<\/td><\/tr>/\n/;
+	    $linea =~ s/&nbsp;/\ /g;
+	    $linea =~ s/<[^<]*>//g;
+	    if(length($linea)>0) {
+		#print $linea."\n";
+		$definizione=$linea;
+	    }
+	}
+	if($cont) {
+	    $definizione="continua su http://www.demauroparavia.it/$numero_lemma\n";
+	}
+    }
+    return $definizione;
 }
 
 1;
--- pulpbot/Google.pm
+++ pulpbot/Google.pm
@@ -1,4 +1,4 @@
-# $Id: Google.pm,v 1.1 2004/08/03 09:50:00 giuseppe Exp $
+# $Id: Google.pm,v 1.2 2004/08/03 10:28:15 giuseppe Exp $
 #========================================================================
 # google:     HTTP GET of a Google query. First n links are displayed.
 #
@@ -32,27 +32,26 @@ use HTTP::Request::Common qw(POST);
 use LWP::UserAgent;
 
 sub google {
-	my $N                  =  5;
-	my $query              =  "$_[0]";
-	my @result;
-	my $el;
-	my $URL              =  "www.google.com/search?num=100&q=";
-	my $ua               =  LWP::UserAgent->new();
-	$ua->agent("Mozilla/4.7 [en] (X11; U; Linux 2.2.17 i686)");
-	my $req              =  HTTP::Request->new(GET => "http://".$URL.$query);
-	$req->header(Accept  => "text/html");
-	my $contentResponse  =  $ua->request($req);
-	my $count            =  0;
-	my $stuff            =  $contentResponse->content;
-	#print $stuff;
-	#while ($stuff  =~ m/.*<font color="#008000">(.*\/)\s-\s\d+k/g)
-	$el=-1;
-	while ($stuff  =~ m/.*<p><a href=http:\/\/(.*?)>.*?<\/a><br><font size=-1>/gi)
-	{
-		if (++$count > $N) { last; }
-	      	$result[++$el]="$1\n";
-	}
-	return @result;
+    my $N = 5;
+    my $query = "$_[0]";
+    my @result;
+    my $el;
+    my $URL = "www.google.com/search?num=100&q=";
+    my $ua = LWP::UserAgent->new();
+    $ua->agent("Mozilla/4.7 [en] (X11; U; Linux 2.2.17 i686)");
+    my $req = HTTP::Request->new(GET => "http://".$URL.$query);
+    $req->header(Accept  => "text/html");
+    my $contentResponse = $ua->request($req);
+    my $count = 0;
+    my $stuff = $contentResponse->content;
+    #print $stuff;
+    #while ($stuff  =~ m/.*<font color="#008000">(.*\/)\s-\s\d+k/g)
+    $el=-1;
+    while ($stuff  =~ m/.*<p><a href=http:\/\/(.*?)>.*?<\/a><br><font size=-1>/gi) {
+	if (++$count > $N) { last; }
+	$result[++$el]="$1\n";
+    }
+    return @result;
 }
 
 1;
