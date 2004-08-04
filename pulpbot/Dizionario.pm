f4295a0c965701d47df00828d72c23a410f18ca3
giuseppe <giuseppe>
2004-08-04 12:17:30 +0000

    corretto Dizionario.pm

--- pulpbot/Dizionario.pm
+++ pulpbot/Dizionario.pm
@@ -1,4 +1,6 @@
 package Dizionario;
+use HTTP::Request::Common qw(POST);
+use LWP::UserAgent;
 use Exporter;
 @ISA = qw(Exporter);
 @EXPORT_OK=qw(cerca);
@@ -6,8 +8,18 @@ use Exporter;
 sub cerca {
     #@temp= split /\ /, @_;
     $stringa = $_[0];
-    $numero_lemma = $_[1]; 
-    $numero_pagina=`curl http://www.demauroparavia.it/cerca?stringa=$stringa`; 
+    $numero_lemma = $_[1];
+    $stringa =~ s/^\s+//;
+    $stringa =~ s/\s+$//;
+    my $url="www.demauroparavia.it";
+    my $query="cerca.php?stringa=$stringa";
+    my $agente = LWP::UserAgent->new();
+    $agente->agent("Mozilla/4.7 [en] (X11; U; Linux 2.2.17 i686)");
+    my $richiesta = HTTP::Request->new(GET => "http://$url/$query");
+    $richiesta->header(Accept  => "text/html");
+    my $contenuto = $agente->request($richiesta);
+    my $numero_pagina = $contenuto->content;
+    #$numero_pagina=`curl http://www.demauroparavia.it/cerca.php?stringa=$stringa`; 
     if(!($numero_pagina =~ /Non ho trovato occorrenze/)) {
 	if($numero_pagina =~ /URL/) {
 	    #print("un solo lemma");
@@ -55,7 +67,17 @@ sub cerca {
 	
     if($numero_lemma)
     {
-	$pagina=`curl http://www.demauroparavia.it/$numero_lemma | grep -a class=lemma`; 
+	$query=$numero_lemma;
+	$richiesta = HTTP::Request->new(GET => "http://$url/$query");
+	$richiesta->header(Accept  => "text/html");
+	$contenuto = $agente->request($richiesta);
+	my $pagina = $contenuto->content;
+	my @pagina = split("\n", $pagina);
+	foreach my $riga(@pagina){
+	    if($riga=~/class=lemma/){
+		$pagina=$riga;
+	    }
+	}
 	if(length($pagina)>1500) {
 	    $pagina=substr($pagina,0,1500);
 	    $cont=1;
